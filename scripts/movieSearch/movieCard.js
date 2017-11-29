cc22701009e121a6f1c21caf651af85138b35e23
Kristen Norris <krnorris65@gmail.com>
2017-11-29 13:35:25 -0600

    displays actors in an unordered list

--- scripts/movieSearch/movieCard.js
+++ scripts/movieSearch/movieCard.js
@@ -4,30 +4,47 @@
 const $ = require("jquery")
 
 const movieCard = (movie) => {
+    const movieId = movie.id
 
     $.ajax({
-        "url": `http://api.themoviedb.org/3/movie/${movie.id}/casts?api_key=858deec9a8305f575390bb92f4c3eab8`,
+        "url": `http://api.themoviedb.org/3/movie/${movieId}/casts?api_key=858deec9a8305f575390bb92f4c3eab8`,
         "method": "GET"
-    }).then(cast => {
+    }).then(movieCast => {
+        const castArray = movieCast.cast
+        const posterPath = movie.poster_path
+        const movieYear = movie.release_date.slice(0,4)
+        let movieString = ""
         
-        console.log(cast.cast)
-    })  
-
-    const posterPath = movie.poster_path
-    const movieYear = movie.release_date.slice(0,4)
-    let movieString = ""
-    movieString += `
+        movieString += `
             <section class="movieCard" id="movie_${movie.id}">
                 <span id="delete_${movie.id}">&#10006;</span>
                 <div class="yourMovies">
                     <h1 class="movieName">${movie.title}</h1>
                     <img src="https://image.tmdb.org/t/p/w150${posterPath}" alt="${movie.title} poster" class="moviePoster">
                     <p class="movieRelease">Year released: ${movieYear}</p>
-                    <p class="movieActors">Top Billed Actors: </p> //figure out how these will be stored in the database
-                </div>
-            </section> 
+                    <p>Top Billed Actors: 
+                        <ul class="movieCast">
+                        
         `
-    $("#content").append(movieString)
+        castArray.forEach( topCast => {
+            if(topCast.order < 5) {
+                movieString += `
+                    <li class="actor">${topCast.name}</li>
+                `
+            }
+        })
+
+        movieString += `
+        </ul>        
+        </p>
+        </div>
+        </section> 
+        `
+        
+        $("#content").append(movieString)
+    })  
+
+
 
 }
 
