fc941fa9e85292d7acf62ff66cefa5161b44d1be
Jason Figueroa <jason.figueroa2@gmail.com>
2017-11-29 11:48:39 -0600

    added properties watched and rating to the userMovie object

--- scripts/addMovie/createUserMovieObj.js
+++ scripts/addMovie/createUserMovieObj.js
@@ -3,7 +3,9 @@ const userId = require("../authorization/authorization")
 const createUserMovieObj = (movieId) => {
     return {
         user_id: userId.activeUser.uid,
-        movie_id: movieId
+        movie_id: movieId,
+        watched: false,
+        rating: null
     }
 }
 
