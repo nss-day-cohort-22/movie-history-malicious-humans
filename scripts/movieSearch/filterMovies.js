62d9dfdcaeed4fa5f85c9159d8846a08ebe60d44
Tyler <bowmant388@gmail.com>
2017-11-30 11:08:09 -0600

    merged

--- scripts/movieSearch/filterMovies.js
+++ scripts/movieSearch/filterMovies.js
@@ -1,64 +1,79 @@
 //author: Kristen
-//functionality: allows user to filter through 
+//functionality: allows user to filter through their movies to find a specific movie
 
 const $ = require("jquery")
 
-const filterPage = (func) => {
-    const searchEl = $("#searchYourMovies")
+const filterPage = (movieArray) => {
+    const searchEl = $("#findNewMovies")
     const contentEl = $("#content")
-    const movieCard = $(".movieCard")
-    
-    const movieArray = $.makeArray(movieCard)
-    const movieName = $.movieArray.children()
-    debugger
-    let pageLoad = ""
-    
-    if (movieArray.length > 0) {
-        //create search input
-        const searchBar = "<p>Search: <input type=\"text\" name=\"movieFilter\" placeholder=\"search all\"></p>"
+    //create search input
+    const searchBar = "<p>Find Your Movie: <input type=\"text\" name=\"movieFilter\" placeholder=\"search movie titles\"></p>"
 
-        searchEl.html(searchBar)
+    searchEl.html(searchBar) //adds search bar to DOM
         
-        //targets input to add an eventListener
-        const pageSearch = $("input[name='movieFilter']")[0]
-            
-        pageLoad = func(movieArray) //initial page load of items
-        
-        // pageSearch.addEventListener(
-        //     "keyup",
-        //     event => {
-        //         if(event.target.value.length >= 3) {
-        //             //convert what is being filtered to lowercase
-        //             const userFilterString = event.target.value.toLowerCase()
+    //targets input to add an eventListener
+    const pageSearch = $("input[name='movieFilter']")[0]
 
-        //             let pageFilter = dbArray.filter(filteredItem => {
-        //                 for(key in filteredItem) {
-        //                     const item = filteredItem[key]
+    const searchMovies = function (event) {
+        if(event.target.value.length >= 3) {
+            //convert what is being filtered to lowercase
+            const userFilterString = event.target.value.toLowerCase()
 
-        //                     if(item.toLowerCase().includes(userFilterString)) {
-        //                         return item
-        //                     }
-        //                 }
-        //             })
-
+            let filteredList = movieArray.filter(filteredItem => { 
+                const movieTitle = filteredItem.title //gets the title of the current movie
                 
-        //             //output.content.html(pageLoad) repopulates the content area when user types in search bar
-        //             if(pageFilter.length === 0) {
-        //                 pageLoad = "<h3>Search Results Not Found</h3>"
-        //                 contentEl.html(pageLoad) 
-        //             } else {
-        //                 pageLoad = func(pageFilter) //displays filtered items
-        //                 contentEl.html(pageLoad)
-        //             }
-        //         } else {
-        //             pageLoad= func(dbArray) //displays initial page load if selector has less than three characters
-        //             contentEl.html(pageLoad)
-        //         }
-        //     }
-        // )
-        
+                const containsString = movieTitle.toLowerCase().includes(userFilterString) //determines if the movieTitle contains what was searched for (returns true or false)
+                const filteredMovie = $(`#movie_${filteredItem.id}`) //gets the corresponding movieCard
+                
+                if(containsString === false) {
+                    filteredMovie.addClass("hidden") //if the title is not what is searched for add a class of hiddne
+                } else {
+                    if(filteredMovie.hasClass("hidden")) { //if the title is what was searched for but has a class of hidden, remove the class
+                        filteredMovie.removeClass("hidden")
+                    }
+                }
+                
+            })
+
+
+        } else {
+            movieArray.filter(filteredItem => { //if the length of the search is less than 3 remove the class of hidden from all movieCards
+                const movie = $(`#movie_${filteredItem.id}`)
+                
+                if(movie.hasClass("hidden")) {
+                    movie.removeClass("hidden")
+                }
+                
+            })
+        }
     }
-    return pageLoad
+            
+        
+    pageSearch.addEventListener("keyup", searchMovies)
+
+    // unwatchedMovie.addClass("hidden") //hide unwatched movies
+                    
+    // if(watchedMovie.hasClass("hidden") === true){ //if the watched movies have a class of hidden, remove that class
+    //     watchedMovie.removeClass("hidden")
+    // }
+                
+    //     //output.content.html(pageLoad) repopulates the content area when user types in search bar
+    //     if(pageFilter.length === 0) {
+    //         pageLoad = "<h3>Search Results Not Found</h3>"
+    //         contentEl.html(pageLoad) 
+    //     } else {
+    //         pageFilter.forEach(movie =>
+    //             func(movie)
+    //         )
+    //     }
+    // } else {
+    //     movieArray.forEach(movie =>
+    //         func(movie)
+    //     )
+    // }
+
+        
+
 }
 
 module.exports =  filterPage
